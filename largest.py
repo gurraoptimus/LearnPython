8e93c71f9f3af9f98183088948d007ad99d50f98
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-21 14:14:25 +0100

    LearnPyton

--- largest.py
+++ largest.py
@@ -1,13 +1,12 @@
-a = [1,2,3,4,6,7,99,88,999]
-max_num = 0
-for i in a:
-    if i > max_num:
-        max_num = i
-print(a.index(max_num))
+# a = [1,2,3,4,6,7,99,88,999]
+#  for i in a:
+#  max_num = 0
+    #  if i > max_num:
+        #  max_num = i
+#  print(a.index(max_num))
 
-
-# a = (1, 2, 3, 9)
-# print(max(a))
+a = (1, 2, 3, 9)
+    print(max(a))
 
 # x = max("Oscar","Ozzle","MagicOA")
 # print(x)
