5a3229fb2b4b57cc42e2b1f0dee6f6aa9dea78ed
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-25 17:51:08 +0100

    LearnPython

--- pwd.py
+++ pwd.py
@@ -5,11 +5,11 @@ pwd="abcdefgijklmnopqrstuvw123456789_%&#*"
 ld= input("pwd?:")
 ld= int(ld)
 
-password=""
+passwd=""
 for i in range(ld):
     sl_tk= random.choice(pwd)
     if random.randint(0,1)==0:
         sl_tk=sl_tk.upper()|
     
-    password+= sl_tk
-print(password)
+    passwd+= sl_tk
+print(passwd)
