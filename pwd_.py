a98b4fee084fd660d6a2a556bf583ebd547ef91c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-29 12:44:52 +0100

    LearnPython

--- pwd_.py
+++ pwd_.py
@@ -1,16 +1,17 @@
 import random
 
-pwd ="abcdefgijklmnopqrstuvw123456789_%&#*"
+pwd = "abcdefgijklmnopqrstuvw123456789_%&#*"
 
-ld = input("pwd?:")
+ld = input("How long do you want the password to be?: ")
 ld = int(ld)
 
 passwd = ""
 for i in range(ld):
     sl_tk = random.choice(pwd)
     
-    if random.randint(0,1)==0:
+    if random.randint(0, 1) == 0:
         sl_tk = sl_tk.upper()
 
-passwd += sl_tk
-print(passwd)
+    passwd += sl_tk
+
+print("Your generated password is:", passwd)
