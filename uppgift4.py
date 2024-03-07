8b1a5f4bb11ebae5e396a00d15c533fcc5eea338
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-07 21:06:51 +0100

    pycorse

---
+++ uppgift4.py
@@ -0,0 +1,12 @@
+def belle(numbers)
+    # Kollar att numbers är exakt 3 tal
+    if len(numbers) != 3
+        return "Listan måste ha tre tal!"
+
+    # Hittar den största siffran
+    max_number=max(numbers)
+    return max_number
+
+numbers=[94,96,23]
+largest_number= find_largest_number(numbers)
+print("Det största talet är:", largest_number)
