657b3bbd93dcdc9413acaec43a055b7a69ff0c1a
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-07 20:49:45 +0100

    pycorse

---
+++ up2.py
@@ -0,0 +1,14 @@
+def uppgift2(name, last_name, age):
+    ## User input / Användar data
+    name = input("Your name/Ditt namn:")
+    last_name = input("Your last name/Ditt efternamn:")
+    age = input("Your age/Din ålder:")
+
+    ##Överskrider alltid den befintliga informationen
+    with open("namelist.txt", "w") as file:
+        file.write(f"{name} {last_name},{age}\n")
+        file.close
+
+if __name__ == "__main__":
+    uppgift2("Gustav", "Arnflo", 23)
+    
---
+++ uppgift.py
@@ -0,0 +1,3 @@
+f = open("uppgift.txt", "a")
+f.write("hej fil")
+f.close()
