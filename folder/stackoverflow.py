33c2c9d46dbe5298dc861f5bd5d0799051aa048d
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-07 23:45:38 +0100

    stackoverflow

--- folder/stackoverflow.py
+++ folder/stackoverflow.py
@@ -1,14 +1,14 @@
 #Ask for number input
-first = int(raw_input('Please type a number: '))
-second = int(raw_input('Please type a number: '))
-third = int(raw_input('Please type a number: '))
-fourth = int(raw_input('Please type a number: '))
-fifth = int(raw_input('Please type a number: '))
-sixth = int(raw_input('Please type a number: '))
-seventh = int(raw_input('Please type a number: '))
-eighth = int(raw_input('Please type a number: '))
-ninth = int(raw_input('Please type a number: '))
-tenth = int(raw_input('Please type a number: '))
+first = int(input('Please type a number: '))
+second = int(input('Please type a number: '))
+third = int(input('Please type a number: '))
+fourth = int(input('Please type a number: '))
+fifth = int(input('Please type a number: '))
+sixth = int(input('Please type a number: '))
+seventh = int(input('Please type a number: '))
+eighth = int(input('Please type a number: '))
+ninth = int(input('Please type a number: '))
+tenth = int(input('Please type a number: '))
 
 #create a list for variables
 sorted_list = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth]
@@ -16,6 +16,6 @@ dd_numbers = []
 
     #filter list and add odd numbers to new list
 for value in sorted_list:
-       odd_numbers.append(value) 
-        if value%2 != 0:
-             print ('The greatest odd number you typed was:'), max(odd_numbers)
+    if value%2 != 0:
+        odd_numbers.append(value) 
+print ('The greatest odd number you typed was:'), max(odd_numbers)
