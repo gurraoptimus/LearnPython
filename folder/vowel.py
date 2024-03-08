b118b800de8dc32e808fa600d8407c6341937c86
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-08 11:54:16 +0100

    pycorse

---
+++ folder/vowel.py
@@ -0,0 +1,14 @@
+class msgWriter(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def __enter__(self):
+        self.file = open(self.file_name, "w")
+        return self.file
+
+    def __exit__(self, *args):
+        self.file.close()
+
+    #using with statement with MessageWriter
+    with msgWriter("my_file.txt") as xfile:
+        xfile.Write("this is a txt file")
