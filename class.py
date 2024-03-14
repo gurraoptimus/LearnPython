d77bc2e94c9c004b945fe2ce6e8354e354e7a00d
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-14 20:46:57 +0100

    pycourse

---
+++ class.py
@@ -0,0 +1,3 @@
+class bil:
+    def __init__(self, in_modell, in_miltal):
+        self.modell
---
+++ program.py
@@ -0,0 +1,8 @@
+thisdict = {
+    "brand": "Ford",
+    "model":  "Mustang",
+	"year": 1964
+}
+
+# Assigning a value
+thisdict["year"] = 2018
