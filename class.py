98be41d1bd8f9324dfd2b8f9a656456a93956f1d
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-18 15:08:39 +0100

    pycourse

--- class.py
+++ class.py
@@ -1,3 +1,9 @@
 class bil:
     def __init__(self, in_modell, in_miltal):
-        self.modell
+        self.modell = in_modell
+        self.miltal = in_miltal
+    def print_bil(self):
+        print(self)
+
+b1 = Bil("volvo v70",1005)
+b2 = Bil("saab",1234567)
