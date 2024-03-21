a977c0cd77d489546a4bf6bc7e6c0284edfe3c2f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-21 22:07:35 +0100

    pycourse

--- sitmap.py
+++ sitmap.py
@@ -4,4 +4,12 @@ import requests as re
 from bs4 import BeautifulSoup as bs
 
 data = re.get("pass")
-data_new = bs()
+data_parse = bs(data.content, "xml")
+
+data_find = data_parse.find_all("loc")
+for item in data_find:
+    data_temp = re.get(item.contents[0])
+    data_temp_parse = bs(data_temp.content, "html.parser")
+    text = data_temp_parse.title.text
+    if "pass" in text.lower():
+        print(text)
