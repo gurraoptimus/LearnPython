536d5204df2dffe73fac00e918d3709231cb7130
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-21 22:34:56 +0100

    sitmap2

---
+++ sitmap2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import openpyxl
+import requests as re
+from bs4 import BeautifulSoup as bs
+
+data = re.get("https://gurraoptimus.se/sitmap.xml")
+data_new bs(data.content, "xml")
+print(data_new)
