5e0277cb5dcc6b3a98631c5aa354aa6350565440
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-03-22 17:30:37 +0100

    sitemaps

--- sitemap2.py
+++ sitemap2.py
@@ -3,6 +3,6 @@ import openpyxl
 import requests as re
 from bs4 import BeautifulSoup as bs
 
-data = re.get("https://gurraoptimus.se/sitmap.xml")
+data = re.get("https://gurraoptimus.se/sitemap.xml")
 data_new = bs(data.content, "xml")
 print(data_new)
